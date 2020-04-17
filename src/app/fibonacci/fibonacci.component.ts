fa4dca34a7008e5e5bf74d062e1d73e550d81095
andres herrera <afandres19@gmail.com>
2020-04-17 12:26:26 -0500

    proiyecto terminado

--- src/app/fibonacci/fibonacci.component.ts
+++ src/app/fibonacci/fibonacci.component.ts
@@ -30,6 +30,7 @@ export class FibonacciComponent implements OnInit {
   }
 
   ecuacion() {
+    
     var a = this.A
     var b = this.B
     var c = this.C
@@ -64,8 +65,9 @@ export class FibonacciComponent implements OnInit {
 
     this.x1 = Number(this.x1.toFixed(2));
     this.x2 = Number(this.x2.toFixed(2));
-
-    this.plano();
+    if (this.x2 != 0 && this.x1 != 0 && d > 0) {
+      this.plano();
+    }
     // this.canvas = document.querySelector("#lienzo2");
     // var ctx = this.canvas.getContext("2d");
     // ctx.beginPath();
@@ -79,75 +81,56 @@ export class FibonacciComponent implements OnInit {
   plano() {
     this.canvas = document.querySelector("#lienzo");
     var ctx = this.canvas.getContext("2d");
-
-    var centroX = this.canvas.width / 2;
-    var centroY = this.canvas.height / 2;
+    
+    // var centroX = this.canvas.width / 2;
+    // var centroY = this.canvas.height / 2;
 
     // Dibujar el plano carteciano para ver
     // el efecto scale sobre las coordenadas
-    ctx.moveTo(centroX, 0);
-    ctx.lineTo(centroX, this.canvas.height);
-    ctx.moveTo(0, centroY);
-    ctx.lineTo(this.canvas.width, centroY);
-    ctx.stroke();
 
-    ctx.translate(centroX, centroY);
-
-    ctx.scale(1, 1);
 
     ctx.beginPath();
-    // ctx.arc(20, 20, 20, 0, Math.PI * 2, true);
-    ctx.quadraticCurveTo(200, this.C, this.x2, 200);
+    for (let i = 0; i < this.canvas.height; i += 10) {
+      ctx.moveTo(0, i + 10);
+      ctx.strokeStyle = "#9c9c9c";
+      ctx.lineTo(this.canvas.width, i + 10);
+      ctx.stroke();
+    }
+    for (let t = 0; t < this.canvas.width; t += 10) {
+      ctx.moveTo(t + 10, 0);
+      ctx.strokeStyle = "#9c9c9c";
+      ctx.lineTo(t + 10, this.canvas.height);
+      ctx.stroke();
+    }
+    ctx.closePath();
+
+    ctx.beginPath();
+    ctx.moveTo(0, this.canvas.height / 2);
+    ctx.strokeStyle = "#000";
+    ctx.lineTo(this.canvas.width, this.canvas.height / 2);
     ctx.stroke();
 
-    ctx.font = "14px Arial";
-    ctx.fillText("Hola", 5, 25);
-
-    // ctx.beginPath();
-    // for (let i = 0; i < this.canvas.height; i += 10) {
-    //   ctx.moveTo(0, i + 10);
-    //   ctx.strokeStyle = "#9c9c9c";
-    //   ctx.lineTo(this.canvas.width, i + 10);
-    //   ctx.stroke();
-    // }
-    // for (let t = 0; t < this.canvas.width; t += 10) {
-    //   ctx.moveTo(t + 10, 0);
-    //   ctx.strokeStyle = "#9c9c9c";
-    //   ctx.lineTo(t + 10, this.canvas.height);
-    //   ctx.stroke();
-    // }
-    // ctx.closePath();
-
-    // ctx.beginPath();
-    // ctx.moveTo(0, this.canvas.height / 2);
-    // ctx.strokeStyle = "#000";
-    // ctx.lineTo(this.canvas.width, this.canvas.height / 2);
-    // ctx.stroke();
-
-    // ctx.moveTo(this.canvas.width / 2, 0);
-    // ctx.strokeStyle = "#000";
-    // ctx.lineTo(this.canvas.width / 2, this.canvas.height);
-    // ctx.stroke();
-    // ctx.closePath();
+    ctx.moveTo(this.canvas.width / 2, 0);
+    ctx.strokeStyle = "#000";
+    ctx.lineTo(this.canvas.width / 2, this.canvas.height);
+    ctx.stroke();
+    ctx.closePath();
 
     // ctx.beginPath();
     // ctx.lineWidht = 6;
     // ctx.beginPath();
     // ctx.moveTo(this.x1, this.canvas.width / 2);
-    // ctx.quadraticCurveTo(200, this.C, this.x2.value, 200);
+    // ctx.quadraticCurveTo(200, this.C, this.x2, 200);
     // ctx.stroke();
 
 
-
-
-
-    // ctx.beginPath();
-    // ctx.translate(150,50);
-    // ctx.lineWidht = 10;
-    // ctx.moveTo(20, 20);
-    // ctx.quadraticCurveTo(20, 500, 200, 20);
-    // ctx.strokeStyle = "green";
-    // ctx.stroke();
+    ctx.beginPath();
+    ctx.translate(150, 50);
+    ctx.lineWidht = 10;
+    ctx.moveTo(20, 20);
+    ctx.quadraticCurveTo(20, this.C + 500, this.x2 + 200, 20);
+    ctx.strokeStyle = "green";
+    ctx.stroke();
 
     // ctx.beginPath();
     // ctx.arc(this.x2 + 220, this.C + 180, 5, 0, Math.PI * 2, false);
